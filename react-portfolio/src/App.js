572d11678fe556c0efcadb46d50edb8a4f85cbdc
Tozzettini <lziche@gmail.com>
2024-03-01 17:21:37 +0100

    going back

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,10 +4,7 @@ import { Helmet } from 'react-helmet';
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>Ziche</title>
-        <link rel="icon" type="image/x-icon" href="./favicon - Copy.ico"  />
-      </Helmet>
+      
       {/* Rest of your application content */}
     </div>
   );
